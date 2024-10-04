d41752048a331040275219692e225484ba326e71
Dmitrii Bondarenko <dimitriosbond2@gmail.com>
2024-10-04 15:53:10 +0300

    Add files via upload

--- HackerU Final Assignment/chinook_dbt/models/facts/fact_invoiceline.sql
+++ HackerU Final Assignment/chinook_dbt/models/facts/fact_invoiceline.sql
@@ -1,8 +1,8 @@
-{{ config(materialized='incremental',unique_key='invoiceid') }}
+{{ config(materialized='incremental',unique_key='invoicelineid') }}
 
-SELECT invoice.*
+SELECT invoiceline.*
     ,'{{ run_started_at.strftime ("%Y-%m-%d %H:%M:%S") }}'::timestamp as dbt_time
-FROM {{source('stg','invoice')}}
+FROM {{source('stg','invoiceline')}}
 WHERE 1=1
 {% if is_incremental() %}
 and last_update::timestamp > (select max(last_update) from {{this}})
